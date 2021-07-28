65d086c84cdf3c5d097eab60c5cb8f96bf0dadc4
Zak Barbuto <zbarbuto@nextfaze.com>
2021-07-29 09:11:52 +0930

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+<center>
+
+# TREMOR
+
+  <img src="icon/icon.png" width="150">
+  
+  ### A quake-style drop-down browser you can open from anywhere
+  
+  ## FAQ
+  
+  ### Why?
+  
+  Because I wanted one and couldn't find one
+  
+  ### How?
+  
+  Launch the app ``then CTRL+ALT+` `` to summon browser. Note, it opens on the monitor your cursor is on.
+  
+  ### Can it use my normal chrome profile?
+  
+  Probably but I don't know how yet
+  
+  ### Can it open an address bar instead of google search by default?
+  
+  Yes, I'll do it later. I made this in 30 minutes so didn't really put much thought into features.
+  
+  ### "Tremor"?
+  
+  Quake -> Earthquake -> Tremors.... I dunno, you name it!
+  
+</center>
---
+++ index.js
@@ -0,0 +1,92 @@
+const {
+  app,
+  BrowserWindow,
+  Tray,
+  globalShortcut,
+  screen,
+  Menu,
+  nativeImage,
+} = require('electron');
+const { join } = require('path');
+
+let hasWin = false;
+
+function createWindow() {
+  const electronScreen = screen;
+  const { x, y } = screen.getCursorScreenPoint();
+  // Find the display where the mouse cursor will be
+  // const currentDisplay = screen.getDisplayNearestPoint({ x, y });
+
+  const displays = electronScreen.getAllDisplays();
+  const currentDisplay = screen.getDisplayNearestPoint({ x, y });
+  const size = currentDisplay.workAreaSize;
+
+  let win = new BrowserWindow({
+    x: currentDisplay.bounds.x,
+    y: 0,
+    frame: false,
+    width: size.width,
+    height: size.height / 3,
+  });
+
+  win.loadURL('https://www.google.com');
+
+  // Emitted when the window is closed.
+  win.on('closed', (e) => {
+    // Dereference the window object, usually you would store window
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    // win = null;
+    hasWin = false;
+    win = null;
+
+    // Don't exit electron
+    e.preventDefault();
+  });
+
+  return win;
+}
+
+app.whenReady().then(() => {
+  let win;
+  const ret = globalShortcut.register('Alt+Ctrl+`', () => {
+    if (!hasWin) {
+      win = createWindow();
+      hasWin = true;
+    } else {
+      win.close();
+    }
+  });
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Exit',
+      enabled: true,
+      click: () => {
+        app.quit();
+      },
+    },
+  ]);
+
+  tray = new Tray(
+    nativeImage
+      .createFromPath(
+        join(
+          __dirname,
+          'icon',
+          process.platform === 'win32' ? 'icon.ico' : 'icon.png'
+        )
+      )
+      .resize(
+        process.platform === 'darwin'
+          ? { width: 16, height: 16 }
+          : { width: 128, height: 128 }
+      )
+  );
+  tray.setToolTip('Tremor');
+  tray.setContextMenu(contextMenu);
+});
+
+app.on('window-all-closed', () => {
+  // do nothing - otherwise electron exits
+});
